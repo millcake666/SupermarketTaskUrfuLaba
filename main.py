a7e0bc618bbdce344c98b65bb4d7fd4545f46711
Dmitry Gryaznov <53191191+millcake666@users.noreply.github.com>
2023-12-23 15:57:39 +0500

    wip

--- main.py
+++ main.py
@@ -1,120 +1,129 @@
-import tkinter as tk
-from tkinter import ttk
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget, QLabel, QSpinBox, QPushButton, QHBoxLayout
 import random
 
-class SupermarketSimulation:
+
+class StoreInterface(QMainWindow):
     def __init__(self):
-        self.root = tk.Tk()
-        self.root.title("Supermarket Simulation")
+        super().__init__()
 
-        # Переменные для хранения данных о потребительской корзине
-        self.basket_data = {
-            'магазины 1-3': {'А': 0.5, 'Б': 0.3, 'В': 1, 'Г': 1, 'Д': 1, 'Е': 0.2, 'Ж': 1},
-            'магазины 4-7': {'А': 1, 'Б': 1, 'В': 1.9, 'Г': 0.4, 'Д': 1, 'Е': 0.4, 'Ж': 0.3},
-            'магазины 8-12': {'А': 0.7, 'Б': 0.7, 'В': 2.5, 'Г': 1.5, 'Д': 1, 'Е': 0.5, 'Ж': 0.8}
-        }
+        self.setWindowTitle("Интерфейс магазина")
+        self.resize(400, 500)  # Установка размеров окна
 
-        # Переменные для хранения данных о товарах
-        self.goods_data = {
-            'А': {'срок_хранения': 3, 'объем_перевозки': 1000},
-            'Б': {'срок_хранения': 3, 'объем_перевозки': 800},
-            'В': {'срок_хранения': 21, 'объем_перевозки': 1200},
-            'Г': {'срок_хранения': 90, 'объем_перевозки': 1200},
-            'Д': {'срок_хранения': 3, 'объем_перевозки': 1200},
-            'Е': {'срок_хранения': 365, 'объем_перевозки': 1000},
-            'Ж': {'срок_хранения': 365, 'объем_перевозки': 1000},
-        }
+        # Переменные для хранения данных
+        self.basket_table = None
+        self.customer_intensity_morning_entry = None
+        self.customer_intensity_day_entry = None
+        self.customer_intensity_evening_entry = None
+        self.service_speed_entry = None
+        self.result_label = None
 
-        # Создание интерфейса
-        self.create_widgets()
+        # Создание элементов интерфейса
+        self.init_ui()
 
-    def create_widgets(self):
-        ttk.Label(self.root, text="Количество магазинов:").grid(row=0, column=0)
-        self.num_stores_entry = ttk.Entry(self.root)
-        self.num_stores_entry.grid(row=0, column=1)
+    def init_ui(self):
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
 
-        # Добавление элементов для ввода данных о потребительской корзине
-        ttk.Label(self.root, text="Потребительская корзина:").grid(row=1, column=0, columnspan=2)
+        layout = QVBoxLayout()
 
-        self.basket_frame = ttk.Frame(self.root)
-        self.basket_frame.grid(row=2, column=0, columnspan=2)
+        # Создание таблицы для корзины
+        self.create_basket_table()
+        layout.addWidget(self.basket_table)
 
-        for i, store_range in enumerate(self.basket_data.keys()):
-            ttk.Label(self.basket_frame, text=store_range).grid(row=i, column=0)
-            for j, product in enumerate(self.basket_data[store_range].keys()):
-                ttk.Label(self.basket_frame, text=product).grid(row=i, column=j + 1)
-                entry_var = tk.DoubleVar()
-                entry_var.set(self.basket_data[store_range][product])
-                ttk.Entry(self.basket_frame, textvariable=entry_var, width=5).grid(row=i, column=j + 2)
+        # Добавление полей ввода и кнопки
+        input_layout = QVBoxLayout()
 
-        ttk.Button(self.root, text="Запустить моделирование", command=self.run_simulation).grid(row=3, column=0, columnspan=2)
+        # Утро
+        morning_layout = QHBoxLayout()
+        morning_label = QLabel("Кл./час утро")
+        morning_label.setAlignment(Qt.AlignLeft)  # Выравнивание к левому краю
+        morning_layout.addWidget(morning_label)
+        self.customer_intensity_morning_entry = QSpinBox(self)
+        self.customer_intensity_morning_entry.setRange(0, 999)
+        self.customer_intensity_morning_entry.setValue(100)
+        morning_layout.addWidget(self.customer_intensity_morning_entry)
+        input_layout.addLayout(morning_layout)
 
-        # Добавление элементов для вывода результатов
-        self.result_text = tk.Text(self.root, height=20, width=80)
-        self.result_text.grid(row=4, column=0, columnspan=2)
+        # День
+        day_layout = QHBoxLayout()
+        day_label = QLabel("Кл./час день")
+        day_label.setAlignment(Qt.AlignLeft)  # Выравнивание к левому краю
+        day_layout.addWidget(day_label)
+        self.customer_intensity_day_entry = QSpinBox(self)
+        self.customer_intensity_day_entry.setRange(0, 999)
+        self.customer_intensity_day_entry.setValue(150)
+        day_layout.addWidget(self.customer_intensity_day_entry)
+        input_layout.addLayout(day_layout)
 
-    def run_simulation(self):
-        num_stores = int(self.num_stores_entry.get())
-        customers_per_store = [random.randint(80, 120) for _ in range(num_stores)]
+        # Вечер
+        evening_layout = QHBoxLayout()
+        evening_label = QLabel("Кл./час вечер")
+        evening_label.setAlignment(Qt.AlignLeft)  # Выравнивание к левому краю
+        evening_layout.addWidget(evening_label)
+        self.customer_intensity_evening_entry = QSpinBox(self)
+        self.customer_intensity_evening_entry.setRange(0, 999)
+        self.customer_intensity_evening_entry.setValue(450)
+        evening_layout.addWidget(self.customer_intensity_evening_entry)
+        input_layout.addLayout(evening_layout)
 
-        # Очистка текстового поля
-        self.result_text.delete(1.0, tk.END)
+        # Общая скорость обслуживания
+        speed_layout = QHBoxLayout()
+        speed_label = QLabel("Скорость обслуживания клиентов")
+        speed_label.setAlignment(Qt.AlignLeft)  # Выравнивание к левому краю
+        speed_layout.addWidget(speed_label)
+        self.service_speed_entry = QSpinBox(self)
+        self.service_speed_entry.setRange(0, 999)
+        self.service_speed_entry.setValue(60)
+        speed_layout.addWidget(self.service_speed_entry)
+        input_layout.addLayout(speed_layout)
 
-        # Вывод результатов в текстовое поле
-        self.result_text.insert(tk.END, "Число покупателей в каждом магазине:\n")
-        for i, customers in enumerate(customers_per_store, start=1):
-            self.result_text.insert(tk.END, f"Магазин {i}: {customers} человек\n")
+        layout.addLayout(input_layout)
 
-        # Логика моделирования
-        self.result_text.insert(tk.END, "\nМоделирование за 2 месяца:\n")
-        for month in range(1, 3):
-            self.result_text.insert(tk.END, f"\nМесяц {month}:\n")
-            for day in range(1, 31):
-                self.result_text.insert(tk.END, f"\nДень {day}:\n")
-                for hour in range(8, 22):
-                    self.result_text.insert(tk.END, f"\nЧас {hour}:\n")
-                    for i in range(num_stores):
-                        # Логика обслуживания покупателей
-                        customers_served = random.randint(50, 70)
-                        self.result_text.insert(tk.END, f"Магазин {i + 1}: Обслужено {customers_served} человек\n")
+        calculate_button = QPushButton("Рассчитать", self)
+        calculate_button.clicked.connect(self.calculate)
+        layout.addWidget(calculate_button)
 
-                        # Логика учета товаров
-                        self.calculate_goods_inventory(i, customers_served)
+        self.result_label = QLabel(self)
+        layout.addWidget(self.result_label)
 
-        # Расчет рекомендуемого объема складов, страхового запаса и других параметров
-        self.calculate_recommendations()
+        central_widget.setLayout(layout)
 
-    def calculate_goods_inventory(self, store_index, customers_served):
-        store_key = f"магазины {store_index + 1}"
-        if store_key in self.basket_data:
-            for product, quantity_per_customer in self.basket_data[store_key].items():
-                # Логика учета товаров
-                required_volume = customers_served * quantity_per_customer
-                current_volume = random.randint(800, 1200)
-                after_delivery = current_volume + required_volume
-                recommended_inventory = after_delivery + max(0, (current_volume - after_delivery) * 0.2)
+    def create_basket_table(self):
+        self.basket_table = QTableWidget(self)
+        self.basket_table.setColumnCount(2)
+        self.basket_table.setHorizontalHeaderLabels(["Товар", "Количество"])
 
-                # Вывод результатов в текстовое поле
-                self.result_text.insert(tk.END,
-                                        f"Магазин {store_index + 1}: Товар {product}, Рекомендуемый объем склада: {recommended_inventory}\n")
-        else:
-            print(f"Ошибка: Не найден ключ '{store_key}' в basket_data.")
+        # Пример заполнения таблицы начальными данными
+        initial_data = [("A", 0.5), ("B", 0.3), ("C", 1), ("D", 1), ("E", 0.2), ("F", 1), ("G", 1)]
+        self.basket_table.setRowCount(len(initial_data))
+        for row, data in enumerate(initial_data):
+            self.basket_table.setItem(row, 0, QTableWidgetItem(data[0]))
+            self.basket_table.setItem(row, 1, QTableWidgetItem(str(data[1])))
 
-    def calculate_recommendations(self):
-        total_inventory = 0
-        total_insurance_stock = 0
+    def calculate(self):
+        # Получение данных из полей ввода
+        basket_data = [(self.basket_table.item(row, 0).text(), float(self.basket_table.item(row, 1).text()))
+                       for row in range(self.basket_table.rowCount())]
 
-        for product in self.goods_data.keys():
-            # Расчет общего объема товаров на всех складах
-            total_inventory += sum([random.randint(800, 1200) for _ in range(int(self.num_stores_entry.get()))])
+        intensity_morning_data = self.customer_intensity_morning_entry.value()
+        intensity_day_data = self.customer_intensity_day_entry.value()
+        intensity_evening_data = self.customer_intensity_evening_entry.value()
+        speed_data = self.service_speed_entry.value()
 
-            # Расчет страхового запаса (может быть адаптировано)
-            total_insurance_stock += sum([random.randint(50, 100) for _ in range(int(self.num_stores_entry.get()))])
+        # Пример обработки данных
+        recommended_storage_volume = random.randint(1000, 2000)  # Замените на расчет по вашим формулам
+        recommended_reserve = random.randint(100, 200)  # Замените на расчет по вашим формулам
 
-        # Вывод результатов в текстовое поле
-        self.result_text.insert(tk.END, f"\nОбщий объем товаров на складах: {total_inventory}\n")
-        self.result_text.insert(tk.END, f"Общий страховой запас: {total_insurance_stock}\n")
+        result_text = f"Рекомендуемый объем складов: {recommended_storage_volume} ед.\n"
+        result_text += f"Страховой запас: {recommended_reserve} ед."
+
+        self.result_label.setText(result_text)
+
 
 if __name__ == "__main__":
-    app = SupermarketSimulation()
-    app.root.mainloop()
+    app = QApplication(sys.argv)
+    window = StoreInterface()
+    window.show()
+    sys.exit(app.exec_())
